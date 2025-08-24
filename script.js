a7225ce4eda1894aef434cc0fe10c5a3f59c04b2
Aman Kumar <amankumarmaurya93@gmail.com>
2025-08-24 14:10:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const base_url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+
+
+for(let select of dropdowns){
+    for(currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        select.append(newOption);
+    }
+}
